82c3857ef5ee4bc4c48d4233c0a1394d900767ce
ChAnYaNG97 <790194334@qq.com>
2021-12-12 22:47:41 +0800

    merge

---
+++ schedulers/allox_scheduler/ALLOX_MCMF_scheduler.go
@@ -0,0 +1,264 @@
+package allox_scheduler
+
+import (
+	"DES-go/simulator"
+	"DES-go/util"
+	"fmt"
+	"math"
+)
+
+
+// In Allox, the author build the scheduling problem as a min-cost bipartite graph matching problem
+
+
+// In graph theory, a min-cost bipartite graph matching problem can be transformed into an equal min cost max flow (MCMF) problem
+// Here we use spfa algorithm to find augmenting path until we can't find more to solve MCMF problem
+// Limited by the method proposed in the paper, the complexity reaches O((n * m) ^ 3)
+
+// BTW, another common way to solve the bipartite graph max match problem is KM algorithm, but it's a bit more complicated to implement.
+
+type AlloxScheduler struct {
+	online bool
+	cluster *simulator.Cluster
+	graph *Graph
+
+	allWaitingJobs []*simulator.JobMeta
+
+}
+
+func (a *AlloxScheduler) DoSchedule() {
+
+}
+
+func (a *AlloxScheduler) SetCluster(cluster *simulator.Cluster) {
+	a.cluster = cluster
+}
+
+func (a *AlloxScheduler) OnScheduleEvent(event simulator.ScheduleEvent) {
+	switch e := event.(type) {
+	case *simulator.ScheduleEventJobsArrived:
+		{
+			a.allWaitingJobs = e.JobMetas()
+			a.DoSchedule()
+		}
+
+	case *simulator.ScheduleEventJobsFinished:
+		{
+		}
+	}
+}
+
+func (a *AlloxScheduler) NextActiveScheduleTime() simulator.Time {
+	return simulator.Time(math.Inf(1))
+}
+
+func (a *AlloxScheduler) Name() string {
+	return fmt.Sprintf("AlloxScheduler[online=%v]", a.online)
+}
+
+
+
+// For Graph Build
+type Node struct {
+	name string
+	// 类型可以是 source, sink, job, gpu
+	nodeType string
+}
+
+func NewNode(name string, nodeType string) *Node {
+	return &Node{name: name, nodeType: nodeType}
+}
+
+type Edge struct {
+	name string
+	from, to *Node
+	// 边的容量，随求解的过程实时更新
+	// 准确来说边的容量应该是一个定值，此处capacity用来表示边上的可用容量
+	// 为方便处理没有进行区分
+	capacity float64
+
+	// 边的权重，指边上单位容量上对应的费用
+	weight float64
+
+	// 是否为反向边
+	reversed bool
+
+}
+
+func NewEdge(from, to *Node, capacity, weight float64, reversed bool) *Edge {
+	edge := &Edge{
+		from: from,
+		to: to,
+		capacity: capacity,
+		weight: weight,
+		reversed: reversed,
+	}
+	edge.name = from.name + "->" + to.name
+	return edge
+}
+
+type Graph struct {
+	// 源点和汇点
+	source, sink *Node
+	// 所有节点
+	nodes []*Node
+	// 每个节点的所有出边
+	outs map[Node][]*Edge
+	// 每个边对应的反向边
+	// Edge类型在求解过程中，capacity会发生改变，不能使用Edge作为key, 采用Edge的Name作为key
+	reverse map[string]*Edge
+}
+func NewGraph() *Graph {
+	return &Graph{
+		nodes: make([]*Node, 0),
+		outs:  make(map[Node][]*Edge),
+		reverse: make(map[string]*Edge),
+	}
+}
+
+func (g *Graph) AddSource(node *Node) {
+	g.source = node
+	g.AddNode(node)
+}
+
+func (g *Graph) AddSink(node *Node) {
+	g.sink = node
+	g.AddNode(node)
+}
+
+func (g *Graph) AddNode(node *Node) {
+	g.nodes = append(g.nodes, node)
+}
+
+func (g *Graph) AddEdge(from, to *Node, capacity, weight float64) {
+	e1 := NewEdge(from ,to , capacity, weight, false)
+	e2 := NewEdge(to, from, 0, -weight, true)
+
+	g.outs[*from] = append(g.outs[*from], e1)
+	g.outs[*to] = append(g.outs[*to], e2)
+
+	g.reverse[e1.name] = e2
+	g.reverse[e2.name] = e1
+}
+
+
+type MCMFSolver struct {
+
+	graph *Graph
+	result map[Node]*Node
+
+	maxFlow float64
+	minCost float64
+
+	flow map[Node]float64
+	// 记录最小费用最大流中所有的路径
+	paths [][]*Edge
+	// 记录每个节点的前驱边，用于path记录
+	l map[Node]*Edge
+
+	q util.Queue
+
+	// spfa
+	distance map[Node]float64
+	visit map[Node]bool
+
+}
+
+func NewMCMFSolver(graph *Graph) *MCMFSolver {
+	solver := &MCMFSolver{
+		graph: graph,
+		result: make(map[Node]*Node),
+		flow: make(map[Node]float64),
+		paths: make([][]*Edge, 0),
+		l: make(map[Node]*Edge),
+		distance: make(map[Node]float64),
+		visit: make(map[Node]bool),
+	}
+	solver.q.New()
+	return solver
+}
+
+func (solver *MCMFSolver) Solve() {
+	for solver.spfa() {
+		solver.maxFlow += solver.flow[*solver.graph.sink]
+		solver.minCost += solver.distance[*solver.graph.sink] * solver.flow[*solver.graph.sink]
+		for node := *solver.graph.sink; node != *solver.graph.source; node = *solver.l[node].from {
+			edge := solver.l[node]
+			edge.capacity -= solver.flow[*solver.graph.sink]
+			solver.graph.reverse[edge.name].capacity += solver.flow[*solver.graph.sink]
+		}
+	}
+}
+
+func (solver *MCMFSolver) spfa() bool {
+
+	for _, node := range solver.graph.nodes {
+		solver.distance[*node] = math.Inf(1)
+		solver.visit[*node] = false
+	}
+	solver.visit[*solver.graph.source] = true
+	solver.distance[*solver.graph.source] = 0
+
+	solver.l[*solver.graph.sink] = nil
+	path := make([]*Edge, 0)
+
+	solver.flow[*solver.graph.source] = math.Inf(1)
+	solver.q.Push(*solver.graph.source)
+	for !solver.q.Empty() {
+		u := solver.q.Front().(Node)
+		solver.q.Pop()
+		solver.visit[u] = false
+		for _, e := range solver.graph.outs[u] {
+			v := *e.to
+			w := e.weight
+			f := e.capacity
+
+			if f > 0 && solver.distance[v] > solver.distance[u] + w {
+				solver.distance[v] = solver.distance[u] + w
+				solver.l[v] = e
+				solver.flow[v] = math.Min(solver.flow[u], f)
+
+				if !solver.visit[v] {
+					solver.q.Push(v)
+					solver.visit[v] = true
+				}
+			}
+		}
+	}
+	if solver.l[*solver.graph.sink] != nil {
+		for node := *solver.graph.sink; node != *solver.graph.source; node = *solver.l[node].from {
+			// 头插的trick
+			path = append([]*Edge{solver.l[node]}, path...)
+		}
+
+		solver.paths = append(solver.paths, path)
+		return true
+	}
+	return false
+}
+
+// 先在图中进行表示，接下来和框架进行整合
+func(solver *MCMFSolver) GetSchedulingResult() map[string]string {
+	result := make(map[string]string)
+
+	for _, path := range solver.paths {
+		for _, edge := range path {
+			from, to := edge.from, edge.to
+			if from.nodeType == "source" || to.nodeType == "sink" {
+				continue
+			}
+			if !edge.reversed {
+				// Job -> GPU
+				result[from.name] = to.name
+			} else {
+				// GPU -> Job
+				delete(result, to.name)
+			}
+		}
+	}
+	return result
+
+
+
+
+}
---
+++ schedulers/allox_scheduler/ALLOX_MCMF_scheduler_2_test.go
@@ -0,0 +1,94 @@
+package allox_scheduler
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestSolver2(t *testing.T) {
+	file, _ := os.Open("/Users/yangchen/Projects/Graduate/DES-go/cases/case_3600.csv")
+	reader := csv.NewReader(file)
+	records, _ := reader.ReadAll()
+	records = records[1:]
+	records = records[:600]
+	//fmt.Println(records[len(records) - 1])
+
+	g := NewGraph()
+	source := NewNode("source", "source")
+	sink := NewNode("sink", "sink")
+	g.AddSource(source)
+	g.AddSink(sink)
+
+	jobNum := 600
+	gpuNum := 60
+
+
+	// Matrix P in the paper
+	timeMatrix := make([][]float64, jobNum)
+	for i := 0; i < jobNum; i++ {
+		timeMatrix[i] = make([]float64, gpuNum)
+	}
+	for i := 0; i < jobNum; i++ {
+		for j := 0; j < gpuNum; j++ {
+			if j < 10 {
+				t, _ := strconv.Atoi(records[i][2])
+				timeMatrix[i][j] = float64(t)
+			} else if j >= 10 && j < 30 {
+				t, _ := strconv.Atoi(records[i][3])
+				timeMatrix[i][j] = float64(t)
+			} else {
+				t, _ := strconv.Atoi(records[i][4])
+				timeMatrix[i][j] = float64(t)
+
+			}
+		}
+	}
+	jobNodes := make([]*Node, jobNum)
+	gpuSlotNodes := make([]*Node, jobNum * gpuNum)
+
+	for i := 0; i < jobNum; i++ {
+		jobNodes[i] = NewNode("job" + strconv.Itoa(i), "job")
+	}
+
+	for i := 0; i < jobNum; i++ {
+		for j := 0; j < gpuNum; j++ {
+			// gpu-j 's slot i (the i th job on gpu j)
+			gpuSlotNodes[i * gpuNum + j] = NewNode("gpu" + strconv.Itoa(j) + "-" + "slot" + strconv.Itoa(i), "gpu")
+		}
+	}
+	weights := make([][]float64, jobNum)
+	for i := 0; i < jobNum; i++ {
+		weights[i] = make([]float64, jobNum * gpuNum)
+		for j := 0; j < jobNum; j++ {
+			for k := 0; k < gpuNum; k++ {
+				weights[i][j * gpuNum + k] = timeMatrix[i][k] * float64(j + 1)
+			}
+		}
+	}
+
+	for _, node := range jobNodes {
+		g.AddNode(node)
+		g.AddEdge(source, node, 1., 0)
+	}
+
+	for _, node := range gpuSlotNodes {
+		g.AddNode(node)
+		g.AddEdge(node, sink, 1., 0)
+	}
+
+	for i, job := range jobNodes {
+		for j, gpuSlot := range gpuSlotNodes {
+			g.AddEdge(job, gpuSlot, 1., weights[i][j])
+		}
+	}
+	solver := NewMCMFSolver(g)
+	solver.Solve()
+
+	fmt.Println("Minimum JCT:", solver.minCost)
+	fmt.Println("Scheduling result:", solver.GetSchedulingResult())
+
+}
+
